b1b028e2594b0ac07b5fe25f6722fe95f5d50eda
dede <dede@unpad.ac.id>
2026-04-08 21:12:33 +0700

    change tailwind to cdn

--- storage/framework/views/ddb4e7eccc9fd494b41eb6bd0f8b7302.php
+++ storage/framework/views/ddb4e7eccc9fd494b41eb6bd0f8b7302.php
@@ -5,6 +5,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo e(config('app.name', 'LibrarySystem')); ?></title>
     <script src="https://cdn.tailwindcss.com"></script>
+    <script>
+        tailwind.config = {
+            theme: {
+                extend: {
+                    fontFamily: {
+                        sans: ['Plus Jakarta Sans', 'sans-serif'],
+                    },
+                    animation: {
+                        'pulse-slow': 'pulse 3s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+                    }
+                }
+            }
+        }
+    </script>
     <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <style>
